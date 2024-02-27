4f343fb9316a6ff99f482f6cce4b689d9bacde39
williambechard <ury2ok2000@gmail.com>
2024-02-27 18:29:03 +0000

    added folder for api

--- src/main.go
+++ src/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello, World!")
+	num := 10
+	myString := "Hello, World!"
+
+	var index = myString[0]
+	fmt.Printf("%v, %T\n", index, index)
+
+	for i, v := range myString {
+		fmt.Println(i, string(v))
+	}
+
+	println("Hello, World! ", num)
 }

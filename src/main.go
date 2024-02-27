031715c4bbbb780446e9094f465ffec448bf58dd
williambechard <ury2ok2000@gmail.com>
2024-02-27 18:56:03 +0000

    simple local database running

--- src/main.go
+++ src/main.go
@@ -1,17 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "The Alchemist", Author: "Paulo Coelho", Quantity: 10},
+	{ID: "2", Title: "The Little Prince", Author: "Antoine de Saint-Exupéry", Quantity: 5},
+	{ID: "3", Title: "The Da Vinci Code", Author: "Dan Brown", Quantity: 7},
+}
 
 func main() {
-	num := 10
-	myString := "Hello, World!"
+	// setup router
+	router := gin.Default()
 
-	var index = myString[0]
-	fmt.Printf("%v, %T\n", index, index)
+	// setup routes
+	setupRoutes(router)
 
-	for i, v := range myString {
-		fmt.Println(i, string(v))
-	}
+	// run server
+	router.Run("localhost:8080")
+}
 
-	println("Hello, World! ", num)
+func setupRoutes(r *gin.Engine) {
+	r.GET("/books", getBooks)
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
 }
